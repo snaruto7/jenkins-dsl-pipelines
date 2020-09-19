67d8fcd4abc3ed5b10ca9298ba14542b7c1712ed
Shivam Narula <shivam.narula7@gmail.com>
2020-09-19 18:01:05 +0530

    updated vault addr

--- shivamlabs/builds/scripts/app_jumping_monster_build.groovy
+++ shivamlabs/builds/scripts/app_jumping_monster_build.groovy
@@ -28,7 +28,7 @@ pipeline {
         }
         stage('Get Registry Details'){
             environment{
-                VAULT_ADDR = "http://40.65.202.70:8200"
+                VAULT_ADDR = "http://40.70.214.80:8200"
             }
             steps{
                 withCredentials([string(credentialsId: "VAULT_ROLE_ID", variable: "ROLE_ID"), string(credentialsId: "VAULT_SEC_ID", variable: "SEC_ID")]) {
