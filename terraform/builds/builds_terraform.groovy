45132dfff1114441f932084da68853379e6b1efe
Shivam Narula <shivam.narula7@gmail.com>
2020-09-20 12:06:47 +0530

    fixed the parameters

--- terraform/builds/builds_terraform.groovy
+++ terraform/builds/builds_terraform.groovy
@@ -12,15 +12,15 @@ mapApps.collect { map ->
             githubProjectUrl("${map.value.github}")
         }
         parameters {
-            choiceParam( name: 'subscription_id', choices: ['shivam-subs'], description: 'Select the subscription')
-            stringParam( name: 'rg_name', defaultValue: '', description: 'Enter resource group name')
-            stringParam( name: 'rg_location', defaultValue: '', description: 'Enter location')
-            stringParam( name: 'cluster_name', defaultValue: '', description: 'Enter unique name for cluster')
-            stringParam( name: 'dns_prefix', defaultValue: '', description: 'Enter unique name for dns prefix')
-            stringParam( name: 'node_count', defaultValue: '3', description: 'Enter node count for AKS cluster')
-            choiceParam( name: 'vm_size', choices: [ 'Standard_DS1_v2','Standard_D2_v2', 'Standard_D2S_v2', 'Standard_B2s', 'Standard_B2ms'], description: 'Enter size for AKS nodes')
-            stringParam( name: 'environment', defaultValue: '', description: 'Add the type of environment')
-            choiceParam( name: 'Create', choices: ['None'], description: 'Tf infa to be created or destroyed or none')
+            choiceParam( 'subscription_id', ['shivam-subs'],  'Select the subscription')
+            stringParam( 'rg_name',  '',  'Enter resource group name')
+            stringParam( 'rg_location',  '',  'Enter location')
+            stringParam( 'cluster_name',  '',  'Enter unique name for cluster')
+            stringParam( 'dns_prefix',  '',  'Enter unique name for dns prefix')
+            stringParam( 'node_count',  '3',  'Enter node count for AKS cluster')
+            choiceParam( 'vm_size', [ 'Standard_DS1_v2','Standard_D2_v2', 'Standard_D2S_v2', 'Standard_B2s', 'Standard_B2ms'],  'Enter size for AKS nodes')
+            stringParam( 'environment',  '',  'Add the type of environment')
+            choiceParam( 'Create', ['None'],  'Tf infa to be created or destroyed or none')
         }
         definition {
             cps {
