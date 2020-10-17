fead8f77ee6420c3f82ab97c1b9214f174b7f9fb
Shivam Narula <shivam.narula7@gmail.com>
2020-10-17 11:16:57 +0530

    Added categorized views

--- dsl_seed_labs.groovy
+++ dsl_seed_labs.groovy
@@ -49,3 +49,29 @@ job('dsl_seed_tf') {
         }
     }
 }
+
+job('dsl_seed_views'){
+    scm{
+        git {
+            remote {
+                url('https://github.com/snaruto7/jenkins-dsl-pipelines.git')
+                credentials('github-creds')
+            }
+            branches('*/master')
+        }
+    }
+    triggers {
+        upstream('dsl_meta_seed', 'UNSTABLE')
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        dsl {
+            external('./views/views_*.groovy')
+            removeAction('DELETE')
+            removeViewAction('DELETE')
+            additionalClasspath('**/tools/*.groovy')
+        }
+    }
+}
